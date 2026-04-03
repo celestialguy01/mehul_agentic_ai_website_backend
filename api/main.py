a887b9ffa89787628cc00683ebd2df44c7716bc7
celestialguy01 <itsmehul01@gmail.com>
2026-04-03 16:08:15 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -38,7 +38,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict
+    allow_origins=[
+      "http://localhost:5173",
+      "https://mehulai.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
